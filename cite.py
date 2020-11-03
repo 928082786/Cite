433e22042fd8a324eca37e558e77390bb5f77ffe
928082786 <928082786@qq.com>
2020-11-03 12:02:52 +0800

    complete

--- cite.py
+++ cite.py
@@ -9,7 +9,7 @@ Created on Wed Oct 28 10:05:13 2020
 from selenium import webdriver
 import time
 from selenium.webdriver.chrome.options import Options
-import re
+from selenium.webdriver.common.keys import Keys
 import numpy as np
 from bs4 import BeautifulSoup 
 
@@ -19,7 +19,11 @@ manage papers and dig papers ref network
 
 class Cite():
     def __init__(self):
-        pass
+        chrome_options=Options()
+        chrome_options.add_argument('--headless')
+        #self.browser = webdriver.Chrome(options=chrome_options)
+        self.browser = webdriver.Chrome()
+
     
     '''
     search papertitle on semanticscholar, return paper_ref_format and paper_bibtex(latex format)
@@ -27,7 +31,7 @@ class Cite():
     ##############################################################################################
     cite = Cite()
     title = “Adversarial Manipulation of Deep Representations.”
-    ref, bibtex = cite(title)
+    ref, bibtex = get_cite(title)
     
     return 
             ref:Sabour, Sara et al. “Adversarial Manipulation of Deep Representations.” CoRR abs/1511.05122 (2016): n. pag.
@@ -42,27 +46,23 @@ class Cite():
 
     '''
     
-    def get_cite(self, title):
-        chrome_options=Options()
-        chrome_options.add_argument('--headless')
-        browser = webdriver.Chrome(options=chrome_options)
-        #browser = webdriver.Chrome()
-        browser.get('https://www.semanticscholar.org')
-        input_ = browser.find_element_by_xpath('//*[@id="search-form"]/div/div/input')
-        input_.send_keys(title)#传送入关键词
-        button1 = browser.find_element_by_xpath('//*[@id="search-form"]/div/div/button/div/span')
-        button1.click()
-        time.sleep(5)
-        button2 = browser.find_element_by_xpath('//*[@id="main-content"]/div[1]/div/div[1]/div[2]/div[2]/div[4]/button/span[2]')
-        button2.click()
-        bibtex = browser.find_element_by_xpath('/html/body/div[1]/div[1]/div/div/div[1]/div[2]/cite')
-        bibtex = bibtex.text
-        button3 = browser.find_element_by_xpath('//*[@id="app"]/div[1]/div/div/div[1]/div[1]/ul/li[2]/button')
-        button3.click()
-        p_s = browser.find_element_by_xpath('//*[@id="app"]/div[1]/div/div/div[1]/div[2]/cite')
-        ref =  p_s.text
-        browser.quit()
-        return ref, bibtex
+    # def get_cite(self, title):
+    #     #browser = webdriver.Chrome()
+    #     self.browser.get('https://www.semanticscholar.org')
+    #     input_ = self.browser.find_element_by_xpath('//*[@id="search-form"]/div/div/input')
+    #     input_.send_keys(title)#传送入关键词
+    #     button1 = self.browser.find_element_by_xpath('//*[@id="search-form"]/div/div/button/div/span')
+    #     button1.click()
+    #     time.sleep(5)
+    #     button2 = self.browser.find_element_by_xpath("//span[text()='Cite']")
+    #     button2.click()
+    #     bibtex = self.browser.find_element_by_xpath('/html/body/div[1]/div[1]/div/div/div[1]/div[2]/cite')
+    #     bibtex = bibtex.text
+    #     button3 = self.browser.find_element_by_xpath('//*[@id="app"]/div[1]/div/div/div[1]/div[1]/ul/li[2]/button')
+    #     button3.click()
+    #     p_s = self.browser.find_element_by_xpath('//*[@id="app"]/div[1]/div/div/div[1]/div[2]/cite')
+    #     ref =  p_s.text
+    #     return ref, bibtex
     
     def clean_bibtex(self, bibtex):
         mood = ['{', '}', '\n']
@@ -70,7 +70,25 @@ class Cite():
             bibtex = bibtex.replace(i, '')
         return bibtex
     
-    
+    def search_cite_one_time(self, title):
+        input_box = self.browser.find_element_by_xpath('//*[@id="search-form"]/div/div/input')
+        input_box.send_keys(Keys.CONTROL, 'a')
+        input_box.send_keys(title)
+        self.browser.find_element_by_xpath('//*[@id="search-form"]/div/div/button/div/span').click()
+        time.sleep(5)
+        button2 = self.browser.find_element_by_xpath("//span[text()='Cite']")
+        button2.click()
+        bibtex = self.browser.find_element_by_xpath('/html/body/div[1]/div[1]/div/div/div[1]/div[2]/cite')
+        bibtex = bibtex.text
+        button3 = self.browser.find_element_by_xpath('//*[@id="app"]/div[1]/div/div/div[1]/div[1]/ul/li[2]/button')
+        button3.click()
+        p_s = self.browser.find_element_by_xpath('//*[@id="app"]/div[1]/div/div/div[1]/div[2]/cite')
+        ref =  p_s.text
+        self.browser.find_element_by_xpath('//*[@id="app"]/div[1]/div/div/div[2]/button/span').click()
+        return ref, bibtex
+        
+        
+        
     '''
     phrase bibtex to convert dict format, and make use through panda.DataFrame
     
@@ -93,20 +111,43 @@ class Cite():
          'ref_paper': ''}
     '''
     
-    def phrase_bibtex(self, ref, bibtex):
+    def parse_bibtex(self, ref, bibtex):
+        '''
+ 
+        Parameters
+        ----------
+        ref : TYPE
+            DESCRIPTION.
+        bibtex : TYPE
+            DESCRIPTION.
+
+        Returns
+        -------
+        parse : TYPE
+            DESCRIPTION.
+            
+        ['id', 'year', 'title', 'ref_papers', 'ref', 'author']
+        '''
+        parse = {}
         bibtex = self.clean_bibtex(bibtex)
-        split_bib = bibtex.split(',')
+        split_bib = bibtex.split(',  ')
         keys = ['id']
         values = [split_bib[0]]
         for item in split_bib[1:]:
             a,b = item.split('=')
-            keys.append(a.strip())
+            keys.append(a)
             values.append(b)
-        phrase = dict(zip(keys, values))
-        phrase['author'] = [i.strip() for i in phrase['author'].split('and')]
-        phrase['ref'] = ref
-        phrase['ref_papers'] = ''
-        return phrase
+        tmp_parse = dict(zip(keys, values))
+        parse['author'] = [i for i in tmp_parse['author'].split('and')]
+        parse['ref'] = ref
+        parse['title'] = tmp_parse['title']
+        parse['year'] = tmp_parse['year']
+        #parse['journal'] = tmp_parse['journal']
+        #parse['volume'] = tmp_parse['volume']
+        parse['id'] = tmp_parse['id']
+
+        
+        return parse
     
     def delay_do(self, browser, x_path):
         try:
@@ -124,30 +165,44 @@ class Cite():
            time.sleep(1)
         return do
     
-    def dig_ref(self, title):
+    
+    def dig_onepaper_refs_title(self, title):
         ref_titles = []
-        chrome_options=Options()
-        chrome_options.add_argument('--headless')
-        browser = webdriver.Chrome(options=chrome_options)
-        #browser = webdriver.Chrome()
-        browser.get('https://www.semanticscholar.org')
-        input_ = browser.find_element_by_xpath('//*[@id="search-form"]/div/div/input')
+        self.browser.get('https://www.semanticscholar.org')
+        input_ = self.browser.find_element_by_xpath('//*[@id="search-form"]/div/div/input')
         input_.send_keys(title)#传送入关键词
-        button1 = browser.find_element_by_xpath('//*[@id="search-form"]/div/div/button/div/span')
+        button1 = self.browser.find_element_by_xpath('//*[@id="search-form"]/div/div/button/div/span')
         button1.click()
-        button2 = self.excute_action(browser, '//*[@id="main-content"]/div[1]/div/div[1]/a')
+        button2 = self.excute_action(self.browser, '//*[@id="main-content"]/div[1]/div/div[1]/a')
         button2.click()
         while(1):
-            time.sleep(3)
-            ref = browser.find_element_by_xpath('//*[@id="references"]/div[2]')
-            bf = BeautifulSoup(ref.get_attribute('innerHTML'), "html.parser")
-            ref = bf.find_all('div', class_ ='cl-paper-title')
-            ref_titles.extend([i.text for i in ref])
-            if len(ref)<10:
+            before_text = ''
+            unload_flag = True#check if cl_paper_title loads
+            while(unload_flag):
+                time.sleep(1)
+                ref = self.excute_action(self.browser,'//*[@id="references"]/div[2]')
+                bf = BeautifulSoup(ref.get_attribute('innerHTML'), "html.parser")
+                ref = bf.find_all('div', class_ ='cl-paper-title')
+                ref_count = bf.find('div', class_='citation-list__label')
+                ref_titles.extend([i.text for i in ref])
+                unload_flag = ref[0].text == before_text
+                before_text = ref[0].text
+            time.sleep(2)
+            if len([i.text for i in ref])<10 or len(self.browser.find_elements_by_link_text("›"))==1:
                 break
-            button3 = browser.find_element_by_xpath('//*[@id="references"]/div[2]/div/div[3]/ul/li[4]/a')
-            time.sleep(3)
-            browser.execute_script("arguments[0].click();", button3)
-        browser.quit()
+            button3 = self.browser.find_elements_by_link_text("›")[-1]
+            self.browser.execute_script("arguments[0].click();", button3)
         return ref_titles
     
+    def dig_onepaper_refs_standard(self, ref_titles):
+        parse = []
+        failures = []
+        for idx in range(len(ref_titles)):
+            try:
+                ref, bibtex = self.search_cite_one_time(ref_titles[idx])
+                parse.append(self.parse_bibtex(ref, bibtex))
+                time.sleep(1)
+            except:
+                failures.append(idx)
+        #parse = parse.extend(self.enhance_refs(ref_titles, failures))
+        return parse
--- main.py
+++ main.py
@@ -8,30 +8,65 @@ Created on Thu Oct 29 07:55:47 2020
 import pandas as pd
 import numpy as np
 import cite
+import collections
 
-def get_paper_ref(title):
-    phrase = []
-    failures = []
-    ref_title = cite.dig_ref(title)
-    for idx in range(len(ref_title)):
-        try:
-            ref, bibtex = cite.get_cite(ref_title[idx])
-            phrase.append(cite.phrase_bibtex(ref, bibtex))
-        except:
-            failures.append(idx)
-            pass
-    
-    return phrase
+def unique_title_df(refs_title):
+    exist_titles = []
+    no_exists_titles = []
+    for item in refs_title:
+        if item in ref_df['title'].tolist():
+            exist_titles.append(item)
+        else:
+            no_exists_titles.append(item)
+            
+    return exist_titles, no_exists_titles
 
-def enhance(failures, ref_title):
-    phrase = []
-    failure
-        
+def unique_id_df(df):
+    se = []
+    for item in df:
+        if not item['id'] in ref_df['id'].tolist():
+            se.append(item)
+    return se
+
+
+def df_where(exist_titles):
+    ref_papers_id = []
+    for item in exist_titles:
+        idx = ref_df[(ref_df['title']==item)]['id'].tolist()
+        ref_papers_id.extend(idx)
+    return ref_papers_id
+
 cite = cite.Cite()
 
 ref_df = pd.read_csv('./ref.csv', index_col=0)
-searched = pd.read_csv('./searched.csv', index_col=0)
-unsearched = pd.read_csv('./unsearched.csv', index_col=0)
+ref_papers = np.load('./ref_paper.npy', allow_pickle=True).tolist()
+searched = pd.read_csv('./searched.csv', index_col=0)['0'].tolist()
+searched = collections.deque(searched)
+unsearched = pd.read_csv('./unsearched.csv', index_col=0)['0'].tolist()
+unsearched = collections.deque(unsearched)
 
-phrase = get_paper_ref(searched['title'])
 
+#cite.__init__()
+while(len(unsearched)):
+    ref_papers_id = []
+    pop_title = unsearched.popleft()
+    refs_title = cite.dig_onepaper_refs_title(pop_title)
+    exist_titles, no_exists_titles = unique_title_df(refs_title)
+    no_exists_standard = cite.dig_onepaper_refs_standard(no_exists_titles)
+    exist_titles_id = [] if exist_titles is None else df_where(exist_titles)
+    ref_papers_id.extend(exist_titles_id)
+    ref_papers_id.extend([item['id'] for item in no_exists_standard]) if len(no_exists_standard)>0 else None
+    ref_papers.append(ref_papers_id)
+    
+    no_exists_standard = unique_id_df(no_exists_standard)
+    ref_df = ref_df.append(no_exists_standard)
+    searched.append(pop_title)
+    unsearched.extend([item['title'] for item in no_exists_standard]) if len(no_exists_standard)>0 else None
+    cite.browser.quit()
+    cite.__init__()
+    
+    print('save')
+    ref_df.to_csv('./ref.csv')
+    pd.DataFrame([i for i in searched]).to_csv('./searched.csv')
+    pd.DataFrame([i for i in unsearched]).to_csv('./unsearched.csv')
+    np.save('./ref_paper.npy', np.array(ref_papers))  
